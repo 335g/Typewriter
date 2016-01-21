d93cd5441f0bfacdd686a713b3719689615a078d
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-21 22:49:10 +0900

    First commit

---
+++ Typewriter/Typewriter.h
@@ -0,0 +1,19 @@
+//
+//  Typewriter.h
+//  Typewriter
+//
+//  Created by Yoshiki Kudo on 2016/01/21.
+//  Copyright © 2016年 Yoshiki Kudo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Typewriter.
+FOUNDATION_EXPORT double TypewriterVersionNumber;
+
+//! Project version string for Typewriter.
+FOUNDATION_EXPORT const unsigned char TypewriterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Typewriter/PublicHeader.h>
+
+
