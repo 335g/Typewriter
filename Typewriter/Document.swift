3c5de9a96fae7eda7a3b57884810af71dae2fd25
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-23 05:47:12 +0900

    Add DocumentType

--- Typewriter/Document.swift
+++ Typewriter/Document.swift
@@ -1,3 +1,12 @@
 //  Copyright © 2016 Yoshiki Kudo. All rights reserved.
 
+import Prelude
+
+// MARK: - DocumentType
+
+public protocol DocumentType: Monoid {
+	static var empty: Self { get }
+	static func char(x: Character) -> Self
+	func beside(other: Self) -> Self
+}
 
