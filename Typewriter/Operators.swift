b5b06f5b5150ee6753caf452f4d099bb4ee9df8a
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-23 06:14:19 +0900

    Add operators

---
+++ Typewriter/Operators.swift
@@ -0,0 +1,43 @@
+//  Copyright © 2016 Yoshiki Kudo. All rights reserved.
+
+///
+/// + : space
+/// - : empty
+/// / : line break
+///
+
+///
+infix operator <> {
+associativity right
+precedence 160
+}
+
+/// space
+infix operator <+> {
+associativity right
+precedence 160
+}
+
+/// line
+infix operator </+> {
+associativity right
+precedence 150
+}
+
+/// linebreak
+infix operator </-> {
+associativity right
+precedence 150
+}
+
+/// softline
+infix operator <+/+> {
+associativity right
+precedence 150
+}
+
+/// softbreak
+infix operator <-/-> {
+associativity right
+precedence 150
+}
