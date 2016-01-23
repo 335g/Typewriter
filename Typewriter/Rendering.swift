c3989e4d91e913010f38bfa632609c673e74e265
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-23 10:20:15 +0900

    RenderedDocument confirm to CustomStringConvertible

--- Typewriter/Rendering.swift
+++ Typewriter/Rendering.swift
@@ -2,12 +2,29 @@
 
 // MARK: - RenderedDocument
 
-public indirect enum RenderedDocument {
+public indirect enum RenderedDocument: CustomStringConvertible {
 	case Fail
 	case Empty
 	case Char(Character, RenderedDocument)
 	case Text(String, RenderedDocument)
 	case Line(Int, RenderedDocument)
+}
+
+// MARK: RenderedDocument : CustomStringConvertible
+
+extension RenderedDocument {
+	public var description: String {
+		switch self {
+		case .Fail, .Empty:
+			return ""
+		case let .Char(c, doc):
+			return String(c) + doc.description
+		case let .Text(s, doc):
+			return s + doc.description
+		case let .Line(i, doc):
+			return "\n\(indentation(i))" + doc.description
+		}
+	}
 }
 
 // MARK: - RenderingRule
