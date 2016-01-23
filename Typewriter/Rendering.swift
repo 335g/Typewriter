7fba128f85a82a3ab9758f098f191f427a4e5e6c
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-23 09:32:37 +0900

    Add RenderedDocument

--- Typewriter/Rendering.swift
+++ Typewriter/Rendering.swift
@@ -1,3 +1,13 @@
 //  Copyright © 2016 Yoshiki Kudo. All rights reserved.
 
+// MARK: - RenderedDocument
+
+public indirect enum RenderedDocument {
+	case Fail
+	case Empty
+	case Char(Character, RenderedDocument)
+	case Text(String, RenderedDocument)
+	case Line(Int, RenderedDocument)
+}
+
 
