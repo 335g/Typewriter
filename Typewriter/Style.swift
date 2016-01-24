fc8d76e94a00de1b7b7e9f0207f7b1f879274894
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-24 20:21:23 +0900

    Add Intensity/Underline/Blink

--- Typewriter/Style.swift
+++ Typewriter/Style.swift
@@ -12,4 +12,31 @@ public extension DocumentStyleType {
 
 public struct DocumentStyle: DocumentStyleType {
 	
+	public enum Intensity: UInt8, StylePropertyType {
+		case Bold = 1
+		case Faint = 2
+	}
+	
+	public enum Underline: UInt8, StylePropertyType {
+		case Single = 4
+		case Double = 21
+	}
+	
+	public enum Blink: UInt8, StylePropertyType {
+		case Slow = 5
+		case Rapid = 6
+	}
+	
+	
+}
+
+public protocol StylePropertyType: RawRepresentable {
+	typealias RawValue = UInt8
+	var code: [RawValue] { get }
+}
+
+public extension StylePropertyType {
+	public var code: [RawValue] {
+		return [self.rawValue]
+	}
 }
