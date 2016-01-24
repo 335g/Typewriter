90614babd0a9671ceb704ef388f4b06ab5fa5b70
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-01-24 16:07:50 +0900

    Remove one of the prettyString

--- Typewriter/Rendering.swift
+++ Typewriter/Rendering.swift
@@ -130,7 +130,3 @@ extension Document {
 public func prettyString(rule: RenderingRule = .Oneline, width: Int, doc: () -> Document) -> String {
 	return doc().prettify(rule, width: width)
 }
-
-public func prettyString(rule: RenderingRule = .Oneline, width: Int, doc: Document) -> String {
-	return doc.prettify(rule, width: width)
-}
