a0497b6e6c2b491dd1f35fadf51be03225c050be
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-02-05 23:40:53 +0900

    Replace Typewritable to DocumentConvertible

--- Typewriter/Conform.swift
+++ Typewriter/Conform.swift
@@ -1,66 +1,66 @@
 //  Copyright © 2016 Yoshiki Kudo. All rights reserved.
 
-// MARK: - Typewritable
+// MARK: - DocumentConvertible
 
-public protocol Typewritable {
-	func pretty() -> Document
+public protocol DocumentConvertible {
+	var document: Document { get }
 }
 
-extension Document: Typewritable {
-	public func pretty() -> Document {
+extension Document: DocumentConvertible {
+	public var document: Document {
 		return self
 	}
 }
 
-// MARK: - DescriptionTypewritable
+// MARK: - SimpleDocumentConvertible
 
-public protocol DescriptionTypewritable: CustomStringConvertible, Typewritable {}
-extension DescriptionTypewritable {
-	public func pretty() -> Document {
+public protocol SimpleDocumentConvertible: CustomStringConvertible, DocumentConvertible {}
+extension SimpleDocumentConvertible {
+	public var document: Document {
 		return try! .text(self.description)
 	}
 }
 
 // MARK: - Adopted: Typewritable
 
-extension String: Typewritable {
-	public func pretty() -> Document {
+extension String: DocumentConvertible {
+	public var document: Document {
 		return .string(self)
 	}
 }
 
-extension String.CharacterView: Typewritable {
-	public func pretty() -> Document {
+extension String.CharacterView: DocumentConvertible {
+	public var document: Document {
 		return .string(String(self))
 	}
 }
 
-extension NSString: Typewritable {
-	public func pretty() -> Document {
+extension NSString: DocumentConvertible {
+	public var document: Document {
 		return .string(String(self))
 	}
 }
 
-extension Int: DescriptionTypewritable {}
-extension Int16: DescriptionTypewritable {}
-extension Int32: DescriptionTypewritable {}
-extension Int64: DescriptionTypewritable {}
-extension Int8: DescriptionTypewritable {}
-extension UInt: DescriptionTypewritable {}
-extension UInt16: DescriptionTypewritable {}
-extension UInt32: DescriptionTypewritable {}
-extension UInt64: DescriptionTypewritable {}
-extension UInt8: DescriptionTypewritable {}
-extension Float: DescriptionTypewritable {}
-extension Double: DescriptionTypewritable {}
-extension Array: DescriptionTypewritable {}
-extension ArraySlice: DescriptionTypewritable {}
-extension Bool: DescriptionTypewritable {}
-extension ClosedInterval: DescriptionTypewritable {}
-extension HalfOpenInterval: DescriptionTypewritable {}
-extension UnicodeScalar: DescriptionTypewritable {}
-extension String.UnicodeScalarView: DescriptionTypewritable {}
-extension String.UTF16View: DescriptionTypewritable {}
-extension String.UTF8View: DescriptionTypewritable {}
-extension Set: DescriptionTypewritable {}
-extension Range: DescriptionTypewritable {}
+extension Int: SimpleDocumentConvertible {}
+extension Int16: SimpleDocumentConvertible {}
+extension Int32: SimpleDocumentConvertible {}
+extension Int64: SimpleDocumentConvertible {}
+extension Int8: SimpleDocumentConvertible {}
+extension UInt: SimpleDocumentConvertible {}
+extension UInt16: SimpleDocumentConvertible {}
+extension UInt32: SimpleDocumentConvertible {}
+extension UInt64: SimpleDocumentConvertible {}
+extension UInt8: SimpleDocumentConvertible {}
+extension Float: SimpleDocumentConvertible {}
+extension Double: SimpleDocumentConvertible {}
+extension Array: SimpleDocumentConvertible {}
+extension ArraySlice: SimpleDocumentConvertible {}
+extension Bool: SimpleDocumentConvertible {}
+extension ClosedInterval: SimpleDocumentConvertible {}
+extension HalfOpenInterval: SimpleDocumentConvertible {}
+extension UnicodeScalar: SimpleDocumentConvertible {}
+extension String.UnicodeScalarView: SimpleDocumentConvertible {}
+extension String.UTF16View: SimpleDocumentConvertible {}
+extension String.UTF8View: SimpleDocumentConvertible {}
+extension Set: SimpleDocumentConvertible {}
+extension Range: SimpleDocumentConvertible {}
